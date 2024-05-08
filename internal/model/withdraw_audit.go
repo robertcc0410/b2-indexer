853a1c7cf173f6e53c2dcb695962af0dee224215
robertcc0410 <132206225+robertcc0410@users.noreply.github.com>
2024-05-08 16:07:21 +0800

    Dev (#142)
    * modify transfer
    
    ---------
    
    Co-authored-by: robertcc <robertcc@test.com>

--- internal/model/withdraw_audit.go
+++ internal/model/withdraw_audit.go
@@ -16,13 +16,14 @@ const (
 
 type WithdrawAudit struct {
 	Base
-	B2TxHash  string `json:"b2_tx_hash" gorm:"type:varchar(256);default:'';comment:b2 network tx hash"`
-	BtcFrom   string `json:"btc_from" gorm:"type:varchar(256);default:'';index"`
-	BtcTo     string `json:"btc_to" gorm:"type:varchar(256);default:'';index"`
-	BtcValue  int64  `json:"btc_value" gorm:"type:bigint;default:0;comment:bitcoin transfer value"`
-	BtcTxHash string `json:"btc_tx_hash" gorm:"type:varchar(256);default:'';comment:bitcoin tx hash"`
-	Status    int    `json:"status" gorm:"type:smallint;default:1"`
-	MPCStatus int    `json:"mpc_status" gorm:"type:smallint;default:1"`
+	APIRequestID string `json:"api_request_id" gorm:"type:varchar(256);comment:withdraw request id"`
+	B2TxHash     string `json:"b2_tx_hash" gorm:"type:varchar(256);default:'';comment:b2 network tx hash"`
+	BtcFrom      string `json:"btc_from" gorm:"type:varchar(256);default:'';index"`
+	BtcTo        string `json:"btc_to" gorm:"type:varchar(256);default:'';index"`
+	BtcValue     int64  `json:"btc_value" gorm:"type:bigint;default:0;comment:bitcoin transfer value"`
+	BtcTxHash    string `json:"btc_tx_hash" gorm:"type:varchar(256);default:'';comment:bitcoin tx hash"`
+	Status       int    `json:"status" gorm:"type:smallint;default:1"`
+	MPCStatus    int    `json:"mpc_status" gorm:"type:smallint;default:1"`
 }
 
 type WithdrawAuditColumns struct {
