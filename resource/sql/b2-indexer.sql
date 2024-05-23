861d13e65175bddb67a967c180053a469c961ad2
robertcc <robertcc@test.com>
2024-05-23 14:27:38 +0800

    add withdraw one-to-many transactions

--- resource/sql/b2-indexer.sql
+++ resource/sql/b2-indexer.sql
@@ -3,5 +3,4 @@ ALTER TABLE sinohope ALTER COLUMN request_id TYPE VARCHAR(128);
 
 -- Handling withdraw one-to-many transactions
 DROP INDEX idx_withdraw_history_b2_tx_hash;
-DROP INDEX idx_withdraw_history_uuid;
 CREATE UNIQUE INDEX idx_unique_b2_tx ON withdraw_history (b2_tx_hash, b2_tx_index, b2_log_index);
